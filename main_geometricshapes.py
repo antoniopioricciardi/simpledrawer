3d7571a47b161b17801f3928a81c7e31d7682e10
Antonio Pio Ricciardi <antoniopioricciardi@pop-os.localdomain>
2021-06-17 10:32:25 +0200

    agent changeable env tested

--- main_geometricshapes.py
+++ main_geometricshapes.py
@@ -45,8 +45,8 @@ config_defaults = {
 }
 
 if __name__ == '__main__':
-    training = True
-    testing = False
+    training = False
+    testing = True
     run = wandb.init(config=config_defaults)  # , project="prova")
     config = wandb.config
     side_length = 5
